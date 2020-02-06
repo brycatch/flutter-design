79ecf4aa9bf0fc89c535ea3b48408e5866eb6b6b
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2020-02-06 09:28:28 -0600

    Terminando diseño

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
+import 'package:designs/src/pages/botones_page.dart';
 import 'package:designs/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
 import 'package:designs/src/pages/basico_page.dart';
+// import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setSystemUIOverlayStyle(
+    //   SystemUiOverlayStyle(statusBarBrightness: Brightness.light),
+    // );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
         'scroll': (BuildContext context) => ScrollPage(),
+        'botones': (BuildContext context) => BotonesPage(), 
       },
     );
   }
